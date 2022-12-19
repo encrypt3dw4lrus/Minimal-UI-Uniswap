fedfcb292c0b917c7577c095532d11da7e1b1223
encrypt3dW4lrus <bulion96@hotmail.com>
2022-12-19 00:12:12 +0000

    changing all settings for QOM

---
+++ src/NavBar/MenuItems.js
@@ -0,0 +1,12 @@
+export const MenuItems = [
+  {
+    title: "Swap",
+    url: "/QL1-Testnet-Minimal/",
+    cName: "nav-links",
+  },
+  {
+    title: "Liquidity",
+    url: "/QL1-Testnet-Minimal/liquidity",
+    cName: "nav-links",
+  },
+];
